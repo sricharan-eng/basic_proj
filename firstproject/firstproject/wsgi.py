e1054eae7e82c999757c646dd505182442b88f4a
sricharan-eng <60461745+sricharan-eng@users.noreply.github.com>
2024-05-04 12:44:02 +0530

    Update wsgi.py

--- firstproject/firstproject/wsgi.py
+++ firstproject/firstproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basic_porj.settings')
 
 application = get_wsgi_application()
